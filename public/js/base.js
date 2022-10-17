7eb404f83d77cf4584e22cc7c696dc6647acbde9
Larissa Martins <larikau2002@gmail.com>
2022-10-17 13:51:01 -0300

    Inicializando o GIT

---
+++ public/js/base.js
@@ -0,0 +1,39 @@
+const nome = "Larissa Martins";
+let nome2 = "";
+let pessoaDeffaoul = {
+    nome:"Larissa Martins",
+    idade:"19",
+    trabalho:"Atendimento"
+}
+
+
+function alterarNome () {
+    nome2 = "Maria Silva";
+    console, log ("'Valor alterado:");
+    console. log (nome2);
+}
+
+function recebeEalteraNome (novoNome) {
+    nome2 = novoNome;
+    console. log ("Valor alterado recebendo um nome:");
+    console. log (nome2);
+}
+
+function imprimirPessoa(pessoa){
+
+}
+
+console.log(pessoa);
+
+console.log("Nome:");
+console.log(pessoa.nome);
+
+console.log("Idade:");
+console.log(pessoa.idade);
+
+console.log("Trabalho:");
+console.log(pessoa.trabalho);
+
+//recebetalteraNome ("João Silva Pereira");
+//recebeEalteraNome ("Maria Silva");
+//alterarNome();
